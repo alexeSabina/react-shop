271f43752ff18a9488922db33a8e9df42df37bd0
Sabina Alexe <sabina.alexe@consultant.LEGO.com>
2019-03-12 16:51:31 +0200

    create product list

--- src/index.js
+++ src/index.js
@@ -2,75 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const defaultImg = require('./assets/not-found.png');
 
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null
-        };
-    }
-    render() {
-      return (
-        <button className="square" onClick={() => this.setState({value: "X"})}>
-          {this.state.value}
-        </button>
-      );
-    }
+class Search extends React.Component {
+  render() {
+    return (
+      <input className="search" placeholder="Search" value={this.props.value} />
+    );
   }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i} />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+}
+
+function ProductImg() {
+  return <img src={defaultImg} alt="not-found" className="item__img"/>;
+}
+
+class Product extends React.Component {  
+  render() {
+    return (
+      <div className="item">
+        <ProductImg />
+        <span className="item__name">
+          {this.props.value}
+        </span>
+      </div>
+    );
+  }
+}
+
+class ProductsList extends React.Component {
+  renderProduct(i) {
+    return <Product value={i}/>
+  }
+
+  render() {
+    return (
+      <div className="content">
+        {this.renderProduct("Casti")}
+        {this.renderProduct("Laptop")}
+        {this.renderProduct("Tablou")}
+        {this.renderProduct("Roti")}
+        {this.renderProduct("Frigider")}
+        {this.renderProduct("Ventilator")}
+        {this.renderProduct("Ciocan")}
+        {this.renderProduct("Cuier")}
+      </div>
+    );
+  }
+}
+
+class Shop extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="container__head">
+          <span className="title">JunkYard</span>
+          <Search />
         </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+
+        <div className="container__list">
+          <ProductsList />
         </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+}
+
+
+
+
+// ========================================
+
+ReactDOM.render(
+  <Shop />,
+  document.getElementById('root')
+);
